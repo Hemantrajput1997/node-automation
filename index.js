5d32c34bd21b00cba29445841ef5c555bb29ba62
hemant <hemant@gmail.com>
2024-04-14 15:15:18 +0530

    CICD

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhHello new changes in index file to test cdi cdNode! qdhqwdh \n'
+  const msg = 'hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhHellow my cicd setup complete changes in index file to test cdi cdNode! qdhqwdh \n'
   res.end(msg);
 });
 
